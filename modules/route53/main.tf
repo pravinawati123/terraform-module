4db270bb9a7e17a000732a17f9eba732158fd2a6
pravinawati123 <awatipravin@gmail.com>
2023-04-03 15:48:43 +0530

    Created Terraform Wirkspace

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,20 +1,3 @@
-  /* data "aws_route53_zone" "hosted_zone" {
-    name = var.domain_name
-} */
-
-  /* resource "aws_route53_record" "site_domain" {
-    zone_id = data.aws_route53_zone.hosted_zone.zone_id
-    name    = var.record_name
-    type    = "CNAME"
-    ttl     = "300"
-    alias {
-      name                   = aws_lb.application_load_balancer.dns_name
-      zone_id                = aws_lb.application_load_balancer.zone_id
-      evaluate_target_health = true
-      }
-  } */
-
-
 resource "aws_route53_zone" "zone" {
   name      = var.domain_name
 }
